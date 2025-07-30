1b12499d0a1caa8491fc4bce9d0deffaa76a787e
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-07-30 01:11:54 +0100

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -19,10 +19,9 @@ const MessageSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  status: {
-    type: String,
-    enum: ['sent', 'delivered', 'read'],
-    default: 'sent'
+  read: {
+    type: Boolean,
+    default: false
   }
 }, { timestamps: true });
 
