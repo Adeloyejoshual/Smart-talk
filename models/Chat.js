538a8b331f072f3fc7f2f86d9bf3bd17750a2b8b
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-07-30 01:11:08 +0100

    Update Chat.js

--- models/Chat.js
+++ models/Chat.js
@@ -1,10 +1,11 @@
+// models/Chat.js
 const mongoose = require('mongoose');
 
-const ChatSchema = new mongoose.Schema(
-  {
-    participants: [String],
-  },
-  { timestamps: true }
-);
+const ChatSchema = new mongoose.Schema({
+  participants: {
+    type: [String], // usernames or user IDs
+    required: true
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Chat', ChatSchema);
