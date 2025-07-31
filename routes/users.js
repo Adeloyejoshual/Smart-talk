4a43b6e4389822cf71bd5929ee8f4daffde5401b
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-01 00:42:08 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const authenticateToken = require("../middleware/auth");
 
 // Middleware to verify token
 function verifyToken(req, res, next) {
