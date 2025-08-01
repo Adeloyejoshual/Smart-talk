2feb3e879c7ffe462d9782f596bdac3c1ff18d2b
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-01 03:16:41 +0100

    Update Chat.js

--- models/Chat.js
+++ models/Chat.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const chatSchema = new mongoose.Schema({
-  from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+const ChatSchema = new mongoose.Schema({
+  from: { type: String, required: true }, // store sender username
+  to: { type: String, required: true },   // store receiver username
   message: { type: String, required: true },
   timestamp: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Chat', chatSchema);
+module.exports = mongoose.model('Chat', ChatSchema);
