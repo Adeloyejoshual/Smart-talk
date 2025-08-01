23474035da2e376d90866cb68432405487879331
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-01 10:51:22 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,31 +1,13 @@
+// models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: false,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  profilePicture: {
-    type: String,
-    default: ''
-  },
-  bio: {
-    type: String,
-    default: ''
-  },
-  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-}, { timestamps: true });
+  username: String,
+  email: String,
+  password: String,
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  online: { type: Boolean, default: false },
+  lastSeen: { type: Date, default: Date.now }
+});
 
 module.exports = mongoose.model('User', userSchema);
