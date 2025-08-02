85f1aea7a2a7f64e5daca661b56dd66726008a4b
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-02 01:16:39 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://adeloyejoshua2020:<db_password>@smarttalk.3gxk7it.mongodb.net/?retryWrites=true&w=majority&appName=Smarttalk';
+const MONGO_URI = 'mongodb+srv://adeloyejoshua2020:MyRealPass123@smarttalk.3gxk7it.mongodb.net/smart-talk?retryWrites=true&w=majority&appName=Smarttalk';
 
-const MONGO_URI = 'mongodb+srv://adeloyejoshua2020:MyRealPass123@smarttalk.3gxk7it.mongodb.net/?retryWrites=true&w=majority&appName=Smarttalk';
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
