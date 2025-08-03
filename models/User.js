3f7605708d041691a0cb0c0c94bbbb85c6dd0ea4
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-03 14:35:44 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,6 +5,8 @@ const UserSchema = new mongoose.Schema({
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
   friends:  [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+reports: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
 });
 
 module.exports = mongoose.model("User", UserSchema);
