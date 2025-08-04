010627d95ebcbc07c3b22e1081572a9553dd0526
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-04 09:24:01 +0100

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,6 +1,17 @@
-// models/Message.js const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-const MessageSchema = new mongoose.Schema( { sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, }, receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, }, content: { type: String, required: true, }, read: { type: Boolean, default: false, }, }, { timestamps: true } );
+const messageSchema = new mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["sent", "delivered", "read"],
+      default: "sent",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Message", MessageSchema);
-
+module.exports = mongoose.model("Message", messageSchema);
