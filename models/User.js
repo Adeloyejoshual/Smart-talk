d68972c84e3e8fe86166db717b77e999904ece82
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-04 15:03:56 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,25 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatar: { type: String, default: "" }, // optional profile picture
+  },
+  { timestamps: true }
+);
 
-userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
+// Hash password before save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-userSchema.methods.comparePassword = async function (plain) {
-  return await bcrypt.compare(plain, this.password);
+// Compare password method
+userSchema.methods.comparePassword = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
 };
 
 module.exports = mongoose.model("User", userSchema);
