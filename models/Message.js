42e773d3966310e143fa1bb91170977fb0f5fa22
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-04 15:00:29 +0100

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -2,10 +2,25 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
   {
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    group: { type: mongoose.Schema.Types.ObjectId, ref: "Group" },
-    content: { type: String, required: true },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+    },
+    content: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
     status: {
       type: String,
       enum: ["sent", "delivered", "read"],
@@ -13,9 +28,6 @@ const messageSchema = new mongoose.Schema(
     },
   },
   { timestamps: true }
-    image: {
-  type: String,
-},
 );
 
 module.exports = mongoose.model("Message", messageSchema);
