66f192512831ec220bcad35e74f1a6d0710b384f
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-04 14:26:06 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+import GroupChat from "./pages/GroupChat";
+
+function App() {
+  return (
+    <div className="App">
+      <GroupChat groupId="123" />
+    </div>
+  );
+}
+
+export default App;
