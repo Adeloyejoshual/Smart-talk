59fcbd6fad4ee33e3bfa1f8a331ff02d4de73823
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-04 14:20:12 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,18 @@
+// src/api.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: import.meta.env.VITE_API,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-API.interceptors.request.use((req) => {
+// Attach token if user is logged in
+API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
-  return req;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export default API;
