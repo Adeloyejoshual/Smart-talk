2ff7261a673904f34dc1cb2fa9f822acd614b982
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-05 22:46:43 +0100

    Update settings.js

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -43,10 +43,4 @@ document.addEventListener("DOMContentLoaded", () => {
       })
       .catch(() => alert("Upload failed"));
   });
-
-  // Logout
-  document.getElementById("logout-btn").addEventListener("click", () => {
-    localStorage.removeItem("token");
-    window.location.href = "/login.html";
-  });
 });
