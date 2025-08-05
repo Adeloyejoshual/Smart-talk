37fc447bf02a3c4de77773844041d0fb9c3f853b
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-08-05 22:26:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
 // MongoDB
 mongoose
