1b9f8dc3807ee3aaf68e7ddb1bbe0280e8b59ddc
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-09-28 12:27:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -210,5 +210,5 @@ io.on('connection', (socket) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
