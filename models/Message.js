3c73c706060df62c2e84d49bc5dedfdfc696f05c
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-09-29 14:58:25 +0100

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -10,91 +10,17 @@ const messageSchema = new mongoose.Schema(
     recipient: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-    },
-    group: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Group",
+      required: true,
     },
     content: {
       type: String,
+      required: true,
     },
     type: {
       type: String,
-      enum: ["text", "image", "file", "system", "voice", "video"],
+      enum: ["text"],
       default: "text",
     },
-    image: {
-      type: String,
-    },
-    file: {
-      type: String,
-    },
-    fileType: {
-      type: String,
-    },
-    status: {
-      type: String,
-      enum: ["sent", "delivered", "read"],
-      default: "sent",
-    },
-    isEdited: {
-      type: Boolean,
-      default: false,
-    },
-    editedAt: {
-      type: Date,
-      default: null,
-    },
-    isDeleted: {
-      type: Boolean,
-      default: false,
-    },
-    deletedFor: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    replyTo: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Message",
-      default: null,
-    },
-    replyCount: {
-      type: Number,
-      default: 0,
-    },
-    emojiReactions: [
-      {
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-        emoji: String,
-      },
-    ],
-    mentions: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    isStarred: {
-      type: Boolean,
-      default: false,
-    },
-    isPinned: {
-      type: Boolean,
-      default: false,
-    },
-    forwardedFrom: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      default: null,
-    },
-    readBy: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
   },
   { timestamps: true }
 );
