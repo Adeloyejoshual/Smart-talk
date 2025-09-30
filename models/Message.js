5d91730c2214dcd9e21006e3aa70578204335520
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-09-30 13:09:46 +0100

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -3,12 +3,11 @@ const mongoose = require("mongoose");
 const MessageSchema = new mongoose.Schema(
   {
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     content: { type: String, default: "" },
-    type: { type: String, enum: ["text", "image", "file"], default: "text" },
+    type: { type: String, enum: ["text","image","file"], default: "text" },
     fileUrl: { type: String, default: "" },
-    fileType: { type: String, default: "text" },
-    status: { type: String, enum: ["sent", "delivered", "read"], default: "sent" }
+    status: { type: String, enum: ["sent","delivered","read"], default: "sent" },
   },
   { timestamps: true }
 );
