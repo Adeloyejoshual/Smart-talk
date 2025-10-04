b726b3f7acf140cc518e25ae04c930d2200c7029
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-04 23:05:54 +0100

    Create server/utils/firebaseAdmin.js

---
+++ server/utils/firebaseAdmin.js
@@ -0,0 +1,18 @@
+import admin from "firebase-admin";
+
+export function initFirebaseAdmin() {
+  if (admin.apps.length) return admin;
+
+  const projectId = process.env.FIREBASE_PROJECT_ID;
+  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY && process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n");
+
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId,
+      clientEmail,
+      privateKey
+    })
+  });
+  return admin;
+}
