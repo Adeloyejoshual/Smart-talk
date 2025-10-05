f11346ef1c7caa8c8cd1ad378d3c466dc801f33f
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-05 01:57:00 +0100

    Update Wallet.js

--- server/models/Wallet.js
+++ server/models/Wallet.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const walletSchema = new mongoose.Schema({
-  uid: { type: String, required: true, unique: true },
+  userId: { type: String, required: true, unique: true },
   balance: { type: Number, default: Number(process.env.NEW_USER_BONUS || 5) },
   currency: { type: String, default: process.env.DEFAULT_CURRENCY || "USD" },
   createdAt: { type: Date, default: Date.now },
+  lastUpdated: { type: Date, default: Date.now },
   expiresAt: { type: Date }
 });
 
