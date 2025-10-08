a122cfe0aa476e648e2d6d103f4b4265185d9b69
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-08 20:09:54 +0100

    Delete utils directory

--- utils/billing.js
+++
@@ -1,8 +0,0 @@
-export const getCallRate = () => {
-  return parseFloat(import.meta.env.VITE_CALL_RATE_PER_SECOND || 0.0033);
-};
-
-export const calculateCost = (durationInSeconds) => {
-  const rate = getCallRate();
-  return parseFloat((durationInSeconds * rate).toFixed(4));
-};
