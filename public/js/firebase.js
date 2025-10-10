cf274f96b63c92b279caa1ccad36629ccf0993a9
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-10 16:15:00 +0100

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,17 +1,19 @@
-<!-- Load Firebase SDKs in each page -->
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
-  const firebaseConfig = {
-    apiKey: "{{FIREBASE_API_KEY}}",
-    authDomain: "{{FIREBASE_AUTH_DOMAIN}}",
-    projectId: "{{FIREBASE_PROJECT_ID}}",
-    storageBucket: "{{FIREBASE_STORAGE_BUCKET}}",
-    messagingSenderId: "{{FIREBASE_MESSAGING_SENDER_ID}}",
-    appId: "{{FIREBASE_APP_ID}}"
-  };
+const app = initializeApp({
+  apiKey: window.ENV.FIREBASE_API_KEY,
+  authDomain: window.ENV.FIREBASE_AUTH_DOMAIN,
+  projectId: window.ENV.FIREBASE_PROJECT_ID,
+  storageBucket: window.ENV.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: window.ENV.FIREBASE_MESSAGING_SENDER_ID,
+  appId: window.ENV.FIREBASE_APP_ID,
+  measurementId: window.ENV.FIREBASE_MEASUREMENT_ID
+});
 
-  window.firebaseApp = initializeApp(firebaseConfig);
-  window.firebaseAuth = getAuth(firebaseApp);
-</script>
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
