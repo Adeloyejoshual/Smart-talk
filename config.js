5a5261c3ae35d5bdf093309155ce0f8e04955ee3
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-10 11:01:17 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window.ENV = {
+  FIREBASE_API_KEY: "AIzaSyB4bwReT2tFQm0I3qjginXREkRITuIzbks",
+  FIREBASE_AUTH_DOMAIN: "my-smarttalk.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "my-smarttalk",
+  FIREBASE_STORAGE_BUCKET: "my-smarttalk.firebasestorage.app",
+  FIREBASE_MESSAGING_SENDER_ID: "111552845929",
+  FIREBASE_APP_ID: "1:111552845929:web:d28d4ab6b1625c53e02af8",
+  FIREBASE_MEASUREMENT_ID: "G-XEQ52BS34R",
+  API_BASE_URL: "https://smart-talk-dqit.onrender.com", // your backend URL
+};
