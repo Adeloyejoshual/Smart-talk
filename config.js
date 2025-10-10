b3e19af672f3b60c5ea933851e5117ed8fe0e192
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-10 15:21:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-window.ENV = {
-  FIREBASE_API_KEY: "AIzaSyB4bwReT2tFQm0I3qjginXREkRITuIzbks",
-  FIREBASE_AUTH_DOMAIN: "my-smarttalk.firebaseapp.com",
-  FIREBASE_PROJECT_ID: "my-smarttalk",
-  FIREBASE_STORAGE_BUCKET: "my-smarttalk.firebasestorage.app",
-  FIREBASE_MESSAGING_SENDER_ID: "111552845929",
-  FIREBASE_APP_ID: "1:111552845929:web:d28d4ab6b1625c53e02af8",
-  FIREBASE_MEASUREMENT_ID: "G-XEQ52BS34R",
-  API_BASE_URL: "https://smart-talk-dqit.onrender.com", // your backend URL
-};
+FIREBASE_API_KEY=AIzaSyBe2QkeEr9sfh0TUtj8Np-mU5WtYYOorYY
+FIREBASE_AUTH_DOMAIN=smarttalk-9fe4a.firebaseapp.com
+FIREBASE_PROJECT_ID=smarttalk-9fe4a
+FIREBASE_STORAGE_BUCKET=smarttalk-9fe4a.firebasestorage.app
+FIREBASE_MESSAGING_SENDER_ID=103759612273
+FIREBASE_APP_ID=1:103759612273:web:311d39bf7af9d51fe30ed0
+FIREBASE_MEASUREMENT_ID=G-XD4MY1XF7E
+FIREBASE_CLIENT_EMAIL=firebase-adminsdk-fbsvc@smarttalk-9fe4a.iam.gserviceaccount.com
