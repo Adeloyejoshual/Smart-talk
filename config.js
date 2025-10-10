caed3d84f4560f96c683ddd7e4df5d97955c9963
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-10 15:27:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-FIREBASE_API_KEY=AIzaSyBe2QkeEr9sfh0TUtj8Np-mU5WtYYOorYY
-FIREBASE_AUTH_DOMAIN=smarttalk-9fe4a.firebaseapp.com
-FIREBASE_PROJECT_ID=smarttalk-9fe4a
-FIREBASE_STORAGE_BUCKET=smarttalk-9fe4a.firebasestorage.app
-FIREBASE_MESSAGING_SENDER_ID=103759612273
-FIREBASE_APP_ID=1:103759612273:web:311d39bf7af9d51fe30ed0
-FIREBASE_MEASUREMENT_ID=G-XD4MY1XF7E
-FIREBASE_CLIENT_EMAIL=firebase-adminsdk-fbsvc@smarttalk-9fe4a.iam.gserviceaccount.com
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
