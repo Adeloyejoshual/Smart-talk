141ad9c88134d9fe3e44470726067b39c600452f
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-10 03:58:42 +0100

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,10 +1,7 @@
+<!-- Load Firebase SDKs in each page -->
 <script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-  import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } 
-    from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
-  import { getFirestore, collection, addDoc, onSnapshot, doc, setDoc } 
-    from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
-  import { getStorage } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-storage.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 
   const firebaseConfig = {
     apiKey: "{{FIREBASE_API_KEY}}",
@@ -15,8 +12,6 @@
     appId: "{{FIREBASE_APP_ID}}"
   };
 
-  const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  export const db = getFirestore(app);
-  export const storage = getStorage(app);
+  window.firebaseApp = initializeApp(firebaseConfig);
+  window.firebaseAuth = getAuth(firebaseApp);
 </script>
