d1b97fefcb2a2a26968479b32e757bc7b30260b1
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-14 11:54:15 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
 import HomePage from "./components/HomePage";
 import ChatPage from "./components/ChatPage";
 import CallPage from "./components/CallPage";
 import SettingsPage from "./components/SettingsPage";
-import IncomingCallListener from "./components/IncomingCallListener";
 
 export default function App() {
   return (
-    <Router>
-      <IncomingCallListener />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/chat/:userId" element={<ChatPage />} />
-        <Route path="/call/:receiverId" element={<CallPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/call" element={<CallPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
