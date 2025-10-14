42a01e71258a76a06a331c462fc5a05eccf9f0c8
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-15 00:02:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,52 @@ import HomePage from "./components/HomePage";
 import ChatPage from "./components/ChatPage";
 import CallPage from "./components/CallPage";
 import SettingsPage from "./components/SettingsPage";
+import CallHistoryPage from "./components/CallHistoryPage";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <ThemeProvider>
       <Router>
         <Routes>
+          {/* Public routes */}
           <Route path="/" element={<HomePage />} />
-          <Route path="/chat" element={<ChatPage />} />
-          <Route path="/call" element={<CallPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected routes */}
+          <Route
+            path="/chat"
+            element={
+              <ProtectedRoute>
+                <ChatPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/call"
+            element={
+              <ProtectedRoute>
+                <CallPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/settings"
+            element={
+              <ProtectedRoute>
+                <SettingsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <ProtectedRoute>
+                <CallHistoryPage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </ThemeProvider>
