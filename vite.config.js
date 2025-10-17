16f691c5e940a732ef693fdb3b0e9b3c23128c7f
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-10-17 22:24:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+// ✅ Vite configuration
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // 👈 matches the server.js static folder
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // This must match the folder Express serves
+  },
+  server: {
+    port: 5173,
   },
 });
