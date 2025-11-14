6bbff40caeedf046c7d9acf462706b4cc15afba3
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-11-14 13:56:14 +0100

    Delete src/awsConfig.js

--- src/awsConfig.js
+++
@@ -1,9 +0,0 @@
-import { S3Client } from "@aws-sdk/client-s3";
-import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
-
-export const s3Client = new S3Client({
-  region: "eu-north-1", // your AWS region
-  credentials: fromCognitoIdentityPool({
-    identityPoolId: "eu-north-1:97d705ec-c1a7-4197-a075-bd18c9c1766f", 
-  }),
-});
