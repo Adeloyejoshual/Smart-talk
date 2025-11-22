fd5215f2fe9ae1768453110251734312e05afe69
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2025-11-22 23:06:50 +0100

    Delete src/components/Chat/ImagePreview.jsx

--- src/components/Chat/ImagePreview.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ImagePreview({ url, onClose }) {
-  return (
-    <div className="image-preview-overlay" onClick={onClose}>
-      <img src={url} alt="preview" />
-    </div>
-  );
-}
